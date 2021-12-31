50d725a6e47bf4e9fd235bc1c6bffa9cf51db0aa
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2021-12-31 09:00:12 -0600

    Fix up config and flesh out categories page

--- src/routes/blog/_post.svelte
+++ src/routes/blog/_post.svelte
@@ -1,6 +1,7 @@
 <script>
   export let title;
   export let date;
+  export let categories;
 </script>
 
 <h1>{title}</h1>
@@ -8,3 +9,18 @@
 <p>Published: {date}</p>
 
 <slot />
+
+{#if categories.length}
+  <aside>
+    <h2>Posted in:</h2>
+    <ul>
+      {#each categories as category}
+        <li>
+          <a href="/blog/categories/{category}">
+            {category}
+          </a>
+        </li>
+      {/each}
+    </ul>
+  </aside>
+{/if}
--- src/routes/blog/categories/[category].svelte
+++ src/routes/blog/categories/[category].svelte
@@ -3,14 +3,35 @@
     const currentCategory = page.params.category
     const response = await fetch('/api/posts.json')
     const posts = await response.json()
-
+    
     const matchingPosts = posts
       .filter(post => post.meta.categories.includes(currentCategory))
 
     return {
       props: {
+        category: currentCategory,
         posts: matchingPosts
       }
     }
   }
 </script>
+
+<script>
+  export let category
+  export let posts = []
+</script>
+
+<h1>Posts in {category}:</h1>
+
+{#if posts.length}
+  {#each posts as post}
+  <h2>
+    <a href={post.path}>
+      {post.meta.title}
+    </a>
+  </h2>
+  Published {post.meta.date}
+  {/each}
+{:else}
+  <h2>Sorry, no posts found matching that category.</h2>
+{/if}
